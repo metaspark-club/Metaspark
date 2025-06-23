f5b6cb5e1a269ce4bd78a0d2918a38bcfef772b2
Hachim E. <69488455+HYYPNNOSS@users.noreply.github.com>
2025-06-24 00:04:24 +0100

    yeah

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -78,5 +78,5 @@ io.on("connection", (socket) => {
   });
 });
 
-// const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
